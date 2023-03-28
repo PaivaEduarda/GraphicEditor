0e3e37643e4728fac86ae1b15d839e1f90f69522
Eduarda Graziele de Paiva <114159027+PaivaEduarda@users.noreply.github.com>
2023-03-28 12:05:33 -0300

    Atualização 28.03.23
    Criamos um novo forms pois o outro deu errado, fizemos os botões abrir, sair e etc

---
+++ Gráfico/Form1.cs
@@ -0,0 +1,58 @@
+﻿using Grafico;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gráfico
+{
+    public partial class frmGeometrico : Form
+    {
+        public frmGeometrico()
+        {
+            InitializeComponent();
+        }
+        private ListaSimples<Ponto> figuras = new ListaSimples<Ponto>();
+        private void pbAreaDesenho_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            figuras.IniciarPercursoSequencial();
+
+            while (figuras.PodePercorrer())
+            {
+                Ponto figuraAtual = figuras.Atual.Info;
+                figuraAtual.Desenhar(figuraAtual.Cor, g);
+            }
+        }
+
+        private void btnCor_Click(object sender, EventArgs e)
+        {
+            dlgCor.ShowDialog();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            if (dlgAbrir.ShowDialog() == DialogResult.OK)
+            {
+                var arquivo = new StreamReader(dlgAbrir.FileName);
+                string linha = "";
+                while (!arquivo.EndOfStream)
+                {
+                    linha = arquivo.ReadLine();
+                }
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
